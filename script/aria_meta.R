15d939d2b0c4139c308edeeff89eda1bcc6efa08
Andrea Borruso <aborruso@gmail.com>
2015-11-01 15:54:02 +0100

    Create aria_meta.R
    Upload file di @patperu per estrazione metadati

---
+++ script/aria_meta.R
@@ -0,0 +1,27 @@
+# uno script R creato da Patrick Hausmann (https://gist.github.com/patperu) per estrarre metadati sui file presenti in questa cartella
+
+options(stringsAsFactors = FALSE)
+
+xpath <- "E:/github/data/aria"
+
+files <- list.files(xpath, recursive = TRUE)
+
+get_data <- function(m) {
+ z <- read.csv2(file.path(xpath, m), nrows = 6, stringsAsFactors = FALSE, head = FALSE)
+ stazione <- z[2, 2]
+ parm <- unique(unlist(z[3, ]))
+ c(stazione, parm)
+ }
+
+zz <- lapply(files, get_data)
+# http://r.789695.n4.nabble.com/How-to-rbind-list-of-vectors-with-unequal-vector-lengths-tp3032489p3032496.html
+zz <- data.frame(t(sapply(zz, '[', 1:max(sapply(zz, length)))))
+zz$date <- unlist(regmatches(files, gregexpr("[0-9]+", files)))
+zz <- zz[order(zz$X1, zz$date), ]
+rownames(zz) <- NULL
+names(zz)[1] <- "stazione"
+zz
+
+#
+# FINE
+#
